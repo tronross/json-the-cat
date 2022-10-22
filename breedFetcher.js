39e421139dd70869abe9d1397a741db6803b2739
tronross <john_ross@analogforever.ca>
2022-10-22 17:02:43 -0400

    cleaned

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,6 +1,7 @@
 const request = require('request');
 const breed = process.argv.slice(2);
 
+// breedFetcher: pulls and parses a database of cat breeds, accepts a breed search from the CLI and logs a description of that breed
 request('https://api.thecatapi.com/v1/breeds/?api_key=live_r0B7cIH8zcZo4kF4ganpZvXVgD9mFC8FinJEY6yL2BpphlLhnt8vEm4sfw8uPseh/search?breed_ids', (error, response, body) => {
   // Print the error if one occurred
   if (error !== null) {
@@ -21,5 +22,3 @@ request('https://api.thecatapi.com/v1/breeds/?api_key=live_r0B7cIH8zcZo4kF4ganpZ
     console.log(`${breed} does not exist in the database`);
   }
 });
-
-
