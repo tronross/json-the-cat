5a840c28e1550aff5cdc9abee18be4b71b29c73a
tronross <john_ross@analogforever.ca>
2022-10-22 17:04:38 -0400

    linted

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,6 +3,7 @@ const breed = process.argv.slice(2);
 
 // breedFetcher: pulls and parses a database of cat breeds, accepts a breed search from the CLI and logs a description of that breed
 request('https://api.thecatapi.com/v1/breeds/?api_key=live_r0B7cIH8zcZo4kF4ganpZvXVgD9mFC8FinJEY6yL2BpphlLhnt8vEm4sfw8uPseh/search?breed_ids', (error, response, body) => {
+  
   // Print the error if one occurred
   if (error !== null) {
     console.log('error:', error);
@@ -16,6 +17,7 @@ request('https://api.thecatapi.com/v1/breeds/?api_key=live_r0B7cIH8zcZo4kF4ganpZ
 
   const data = JSON.parse(body); // parse into array of objects
   const breedObj = data.find(element => element.name === breed[0]); // find breed in array
+  
   if (breedObj) {
     console.log(breedObj.description);
   } else {
