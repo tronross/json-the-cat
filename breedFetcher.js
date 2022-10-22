8313c28afe0b151ac65197757427b5aeee4253f1
tronross <john_ross@analogforever.ca>
2022-10-22 16:45:08 -0400

    Cleaned; implemented logic to handle errors more gracefully

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,29 +1,25 @@
 const request = require('request');
-
 const breed = process.argv.slice(2);
-console.log(breed);
 
-request('https://api.thecatapi.com/v1/breeds/?api_key=live_r0B7cIH8zcZo4kF4ganpZvXVgD9mFC8FinJEY6yL2BpphlLhnt8vEm4sfw8uPseh/search?breed_id=sibe', (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  // console.log('body:', body); 
-  console.log(typeof body);
+request('https://api.thecatapi.com/v1/breeds/?api_key=live_r0B7cIH8zcZo4kF4ganpZvXVgD9mFC8FinJEY6yL2BpphlLhnt8vEm4sfw8uPseh/search?breed_ids', (error, response, body) => {
+  // Print the error if one occurred
+  if (error !== null) {
+    console.log('error:', error);
+    return;
+  }
+  // Print the response status code if not 200
+  if (response.statusCode !== 200) {
+    console.log('statusCode:', response && response.statusCode);
+    return;
+  }
 
-  const data = JSON.parse(body);
-  // console.log(data);
-  console.log(typeof data);
-  // console.log(data[0].description);
-
-  const breedObj = data.find(element => element.name === breed[0]);
-
+  const data = JSON.parse(body); // parse into array of objects
+  const breedObj = data.find(element => element.name === breed[0]); // find breed in array
+  if (breedObj) {
     console.log(breedObj.description);
-  
+  } else {
+    console.log(`${breed} does not exist in the database`);
+  }
 });
 
 
-
-
-
-// const breedObj = function() {
-//   for (let i = 0; i < data.length; i++)
-//     if (data[i].name === breed[0])}
